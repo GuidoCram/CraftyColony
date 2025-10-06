5b8e5725a29303183f6a88101c65edb01fb0c014
Guido Cramwinckel <guido@cramwinckel.nl>
2025-10-06 09:12:29 +0200

    eerste versie van com (leeg) en disk

--- data/computercraft/lua/rom/modules/CraftyColony/core/corecom.lua
+++ data/computercraft/lua/rom/modules/CraftyColony/core/corecom.lua
@@ -1,5 +1,95 @@
 -- define module
 local corecom = {}
 
+-- imports
+
+--[[
+      _                     _       _   _
+     | |                   (_)     | | (_)
+   __| | ___  ___  ___ _ __ _ _ __ | |_ _  ___  _ __
+  / _` |/ _ \/ __|/ __| '__| | '_ \| __| |/ _ \| '_ \
+ | (_| |  __/\__ \ (__| |  | | |_) | |_| | (_) | | | |
+  \__,_|\___||___/\___|_|  |_| .__/ \__|_|\___/|_| |_|
+                             | |
+                             |_|
+
+
+    This modules handles all communications, not used yet but reserved for http and websockets.
+
+--]]
+
+--[[
+      _       _
+     | |     | |
+   __| | __ _| |_ __ _
+  / _` |/ _` | __/ _` |
+ | (_| | (_| | || (_| |
+  \__,_|\__,_|\__\__,_|
+
+--]]
+
+local db	    = {
+    -- local status of shutting down
+	shuttingDown	= false,
+}
+
+--[[
+  _                 _
+ | |               | |
+ | | ___   ___ __ _| |
+ | |/ _ \ / __/ _` | |
+ | | (_) | (_| (_| | |
+ |_|\___/ \___\__,_|_|
+
+--]]
+
+
+--[[
+              _     _ _
+             | |   | (_)
+  _ __  _   _| |__ | |_  ___
+ | '_ \| | | | '_ \| | |/ __|
+ | |_) | |_| | |_) | | | (__
+ | .__/ \__,_|_.__/|_|_|\___|
+ | |
+ |_|
+
+--]]
+
+function corecom.Init()
+	-- nothing to initialize at the moment
+end
+
+function corecom.Setup()
+	-- nothing to setup at the moment
+end
+
+function corecom.Run()
+
+	-- run until we are shutting down
+	while not db.shuttingDown do
+
+        -- if no activity, sleep for a short time to prevent busy waiting
+        os.sleep(0.1)
+	end
+end
+
+function corecom.Shutdown()
+	-- just change the status, nothing more at this point. We need to wait for the work to finish
+	db.shuttingDown = true
+end
+
+
+--[[
+           _
+          | |
+  _ __ ___| |_ _   _ _ __ _ __
+ | '__/ _ \ __| | | | '__| '_ \
+ | | |  __/ |_| |_| | |  | | | |
+ |_|  \___|\__|\__,_|_|  |_| |_|
+
+--]]
+
+
 -- done
 return corecom
--- data/computercraft/lua/rom/modules/CraftyColony/core/coredisk.lua
+++ data/computercraft/lua/rom/modules/CraftyColony/core/coredisk.lua
@@ -1,7 +1,136 @@
 -- coredisk.lua
--- Core Disk IO utilities for CraftyColony
-
 local coredisk = {}
+
+-- imports
+
+
+--[[
+      _                     _       _   _
+     | |                   (_)     | | (_)
+   __| | ___  ___  ___ _ __ _ _ __ | |_ _  ___  _ __
+  / _` |/ _ \/ __|/ __| '__| | '_ \| __| |/ _ \| '_ \
+ | (_| |  __/\__ \ (__| |  | | |_) | |_| | (_) | | | |
+  \__,_|\___||___/\___|_|  |_| .__/ \__|_|\___/|_| |_|
+                             | |
+                             |_|
+
+
+    This modules handles disk io, like reading and writing files.
+
+--]]
+
+--[[
+      _       _
+     | |     | |
+   __| | __ _| |_ __ _
+  / _` |/ _` | __/ _` |
+ | (_| | (_| | || (_| |
+  \__,_|\__,_|\__\__,_|
+
+--]]
+
+local db	    = {
+    -- queue for disk operations
+    operationQueue      = {},           -- writing has no harm in queueing
+
+    -- local status of shutting down
+	shuttingDown	= false,
+}
+
+--[[
+  _                 _
+ | |               | |
+ | | ___   ___ __ _| |
+ | |/ _ \ / __/ _` | |
+ | | (_) | (_| (_| | |
+ |_|\___/ \___\__,_|_|
+
+--]]
+
+local function GetNextOperation()
+    -- something to write?
+    if #db.operationQueue > 0 then return table.remove(db.operationQueue, 1) end
+
+    -- still here, no operation
+    return nil
+end
+
+-- for writing files asynchronously
+local function writeFileAsync(path, mode, data)
+
+    -- check the mode. Anything else then write mode will be handled as append
+    if mode ~= "w" then mode = "a" end
+
+    -- now open the file
+    local file = fs.open(path, mode)
+
+    -- only when we could open the file
+    if file then
+        file.write(data)
+        file.close()
+    end
+end
+
+-- for deleting files asynchronously
+local function deleteFileAsync(path)
+
+    -- check if the file exists first
+    if fs.exists(path) then
+
+        -- delete it!
+        fs.delete(path)
+    end
+end
+
+--[[
+              _     _ _
+             | |   | (_)
+  _ __  _   _| |__ | |_  ___
+ | '_ \| | | | '_ \| | |/ __|
+ | |_) | |_| | |_) | | | (__
+ | .__/ \__,_|_.__/|_|_|\___|
+ | |
+ |_|
+
+--]]
+
+function coredisk.Init()
+	-- nothing to initialize at the moment
+end
+
+function coredisk.Setup()
+	-- nothing to setup at the moment
+end
+
+function coredisk.Run()
+
+    -- run until we are shutting down
+    while not db.shuttingDown do
+
+        -- get the next operation
+        local operation = GetNextOperation()
+
+        -- something to do?
+        if operation then
+
+            -- what kind of operation?
+            if operation.operation == "delete" then
+
+                -- let's delete!
+                deleteFileAsync(operation.path)
+
+            elseif operation.operation == "write" then
+
+                -- let's write!
+                writeFileAsync(operation.path, operation.mode, operation.data)
+            end
+        else
+
+            -- currently nothing to do, just sleep a bit
+            os.sleep(0.1)
+        end
+    end
+end
 
 -- Reads the entire contents of a file from disk
 function coredisk.readFile(path)
@@ -16,24 +145,16 @@ end
 
 -- Writes data to a file on disk (overwrites existing)
 function coredisk.writeFile(path, data)
-    local file = fs.open(path, "w")
-    if not file then
-        return false, "Unable to open file for writing: " .. path
-    end
-    file.write(data)
-    file.close()
-    return true
+
+    -- handle later
+    table.insert(db.operationQueue, {operation="write", path=path, mode="w", data=data})
 end
 
 -- Appends data to a file on disk
 function coredisk.appendFile(path, data)
-    local file = fs.open(path, "a")
-    if not file then
-        return false, "Unable to open file for appending: " .. path
-    end
-    file.write(data)
-    file.close()
-    return true
+
+    -- handle later
+    table.insert(db.operationQueue, {operation="write", path=path, mode="a", data=data})
 end
 
 -- Checks if a file exists
@@ -43,11 +164,9 @@ end
 
 -- Deletes a file
 function coredisk.delete(path)
-    if fs.exists(path) then
-        fs.delete(path)
-        return true
-    end
-    return false, "File does not exist: " .. path
+
+    -- handle later
+    table.insert(db.operationQueue, {operation="delete", path=path})
 end
 
 return coredisk
